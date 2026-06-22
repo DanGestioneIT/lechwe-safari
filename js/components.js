10b5432a20abb476568c703a8de9a97cd3ce7d16
dan <dan>
2026-06-22 09:44:41 +0200

    Promo: slower 1.6s zoom-in; footer: move version to its own line

--- js/components.js
+++ js/components.js
@@ -4,7 +4,7 @@
 
 const LOGO = 'images/logo-web.png';
 const WA_NUMBER = '+256760288509';
-const BUILD = '20260622.0941';   // aggiornato automaticamente da deploy.sh ad ogni pubblicazione
+const BUILD = '20260622.0944';   // aggiornato automaticamente da deploy.sh ad ogni pubblicazione
 
 const PROMO = {
   active: true,
@@ -214,7 +214,7 @@ function renderFooter(depth = 0) {
   </div>
   <div class="container">
     <div class="footer-bottom">
-      <p>© ${new Date().getFullYear()} (Ver.${BUILD}) Lechwe Safari. All rights reserved. | Kampala, Uganda</p>
+      <p>© ${new Date().getFullYear()} Lechwe Safari. All rights reserved. | Kampala, Uganda<br><span style="font-size:0.8rem;opacity:0.7;">Ver.${BUILD}</span></p>
       <div class="footer-social">
         <a href="#" class="social-link" title="Facebook">f</a>
         <a href="#" class="social-link" title="Instagram">in</a>
